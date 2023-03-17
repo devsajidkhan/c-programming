ead31903d5511c034dfff29938213f11567cdcda
Sajid Khan <97900799+devsajidkhan@users.noreply.github.com>
2023-03-18 02:20:33 +0500

    created first c program

---
+++ first-program.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    char message[] = "hello world"; // declaring and initializing the variable message
+    printf("%s\n", message); // using format specifier %s instead of hard-coding the message 
+    return 0;
+}
